c2fd8238d02f2657850ac2c0b8b472ac78aaa769
happyblue123 <ngyongxianyx@gmail.com>
2025-03-23 16:06:35 +0800

    rm headerwlogout

--- app/views/about.php
+++ app/views/about.php
@@ -17,11 +17,7 @@ $login = isset($_SESSION['userid']);
 </head>
 <body>
     <?php
-    if ($login) {
-        include "inc/headerwlogout.inc.php";
-    } else {
-        include "inc/header.inc.php";
-    }
+    include "inc/header.inc.php";
     ?>
 
 
