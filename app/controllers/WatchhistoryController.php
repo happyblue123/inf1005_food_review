c95699463b432a2fd563c35f914465b4a1d16efc
happyblue123 <ngyongxianyx@gmail.com>
2025-04-03 17:57:12 +0800

    case sen error for file

--- app/controllers/WatchhistoryController.php
+++ app/controllers/WatchhistoryController.php
@@ -3,7 +3,7 @@ if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 
-require_once __DIR__ . '/../models/Watchhistory.php';
+require_once __DIR__ . '/../models/WatchHistory.php';
 require_once __DIR__ . '/../models/Movie.php';
 
 class WatchHistoryController {
